79545a01a5b3a5accbb51f3b6435b8f2623cab74
datouyu123 <391426555@qq.coom>
2015-01-28 11:01:52 +0800

    Yao 分支提交

--- Mengqiu Pet/Mengqiu Pet/FirstViewController.m
+++ Mengqiu Pet/Mengqiu Pet/FirstViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [NSThread sleepForTimeInterval:3.0];//延长3秒
     // Do any additional setup after loading the view, typically from a nib.
 }
 
